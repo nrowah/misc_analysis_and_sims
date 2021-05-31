3777156e3aa382ff867534f4c78c834d3e0bb85b
nrowah <nrowah@gmail.com>
2021-05-30 17:04:09 -0700

    Create a script for analyzing the landsailer from Veritasium's video

---
+++ propeller_landsailer/landsailer.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Create an analysis of the landsailer featured in: 
+    https://www.youtube.com/watch?v=jyQwgBAaBag
+Created on Sun May 30 16:49:50 2021
+Author: Nathan Rower
+"""
